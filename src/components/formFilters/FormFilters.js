17996a4115c317ef273f93a0f56c7527cf94dec7
jzolotar <zolotarjakub@gmail.com>
2022-06-26 22:17:52 +0200

    start working on form logic

--- src/components/formFilters/FormFilters.js
+++ src/components/formFilters/FormFilters.js
@@ -1,29 +1,51 @@
 import { useState } from 'react';
 
 const FormFilters = () => {
-  const [formValues, setFormValues] = useState({ gender: '', culture: '' });
+  const [culture, setCulture] = useState('');
+  const [gender, setGender] = useState('');
 
-  const onSelectHandler = (event) => {};
+  const onSubmitHandler = (event) => {
+    event.preventDefault();
+    if (culture.trim() === '' || gender === '') {
+      alert('please set filters field');
+    } else {
+      setCulture('');
+      setGender('');
+      event.target.reset();
+      //dispatch action to the store to set filters
 
-  const onInputHandler = (event) => {};
-  const onSubmitHandler = () => {};
+      //filtersON
+      //gender
+      //culture
+    }
+  };
   return (
     <section>
-      <form>
+      <form onSubmit={onSubmitHandler}>
         <div>
           <label>Gender:</label>
-          <select name='gender' id='gender'>
+          <select
+            onChange={(event) => setGender(event.target.value)}
+            name='gender'
+            id='gender'
+          >
             <option value=''>Select gender...</option>
-            <option value='male'>Male</option>
-            <option value='female'>Female</option>
+            <option value='Male'>Male</option>
+            <option value='Female'>Female</option>
           </select>
         </div>
         <div>
           <label>Culture:</label>
-          <input type='text' placeholder='Braavosi' />
+          <input
+            onChange={(event) => setCulture(event.target.value)}
+            type='text'
+            placeholder='Braavosi'
+            value={culture ?? ''}
+          />
         </div>
         <button type='submit'>Apply filters</button>
       </form>
+      <button>Remove filters</button>
     </section>
   );
 };
